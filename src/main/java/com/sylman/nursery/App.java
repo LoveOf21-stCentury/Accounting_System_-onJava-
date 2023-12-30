d3867efbecdb08464ab080564546ec1d5e68b15c
Lyubov <l.musilmanbekova12@gmail.com>
2023-12-30 14:32:30 +0300

    creating and config .gitignore

---
+++ src/main/java/com/sylman/nursery/App.java
@@ -0,0 +1,13 @@
+package com.sylman.nursery;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
