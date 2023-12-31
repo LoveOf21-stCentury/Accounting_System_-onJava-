73c4da808ef711e6d1cc0536f5a982f2d3724a6e
Lyubov <l.musilmanbekova12@gmail.com>
2023-12-31 17:39:41 +0300

    added menu option exceptions

---
+++ src/main/java/com/sylman/nursery/human_friends/presenter/exceptions/MenuOptionException.java
@@ -0,0 +1,7 @@
+package com.sylman.nursery.human_friends.presenter.exceptions;
+
+public class MenuOptionException extends Exception{
+    public MenuOptionException(String message){
+        super(message);
+    }
+}
