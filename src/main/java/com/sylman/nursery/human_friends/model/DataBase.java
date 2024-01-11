c6e003f47d74313c5033ad4d5037ceb994e99300
Lyubov <l.musilmanbekova12@gmail.com>
2024-01-11 16:53:43 +0300

    clear useless lines of code

--- src/main/java/com/sylman/nursery/human_friends/model/DataBase.java
+++ src/main/java/com/sylman/nursery/human_friends/model/DataBase.java
@@ -5,20 +5,16 @@ import com.sylman.nursery.human_friends.model.animals.*;
 import java.sql.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class DataBase {
 
     //    For educational purposes, we will leave these data as CONSTANTS here.
-//    In a real project, this data would be stored in a separate external file
+    //    In a real project, this data would be stored in a separate external file
     private static final String URL = "jdbc:mysql://localhost:3306/nurserydb";
 
     private static final String USERNAME = "root";
     private static final String PASSWORD = "admin";
-
-    private static Connection connection;
 
     private Scanner scanner;
 
@@ -44,11 +40,12 @@ public class DataBase {
         System.out.println("2. Dog");
         System.out.println("3. Hamster");
         System.out.println("4. Camel");
-        // Add more options if needed
+        System.out.println("5. Donkey");
+        System.out.println("6. Horse");
 
         Animals animal = null;
         int choice = scanner.nextInt();
-        scanner.nextLine(); // Consume the leftover newline
+        scanner.nextLine();
 
         switch (choice) {
             case 1:
@@ -63,52 +60,32 @@ public class DataBase {
             case 4:
                 animal = new Camel(name, dateBirth, skills);
                 break;
-            // Add more cases as needed
+            case 5:
+                animal = new Donkey(name, dateBirth, skills);
+                break;
+            case 6:
+                animal = new Horse(name, dateBirth, skills);
+                break;
+
             default:
                 System.out.println("Invalid option selected.");
                 return null;
         }
 
-        // Insert the data into the database
         insertAnimalIntoDB(animal);
 
-        // Return the constructed animal object
         return animal;
     }
 
-//    private Animals chooseAnimalType(String name, LocalDate dateBirth, String skills) {
-//        System.out.println("Select the class: ");
-//        System.out.println("1. Cat");
-//        System.out.println("2. Dog");
-//        System.out.println("3. Hamster");
-//        System.out.println("4. Camel");
-//        // ... other options
-//        int choice = scanner.nextInt();
-//        scanner.nextLine(); // Consume newline
-//
-//        switch (choice) {
-//            case 1:
-//                return new Cat(name, dateBirth, skills);
-//            case 2:
-//                return new Dog(name, dateBirth, skills);
-//            case 3:
-//                return new Hamster(name, dateBirth, skills);
-//            case 4:
-//                return new Camel(name, dateBirth, skills);
-//            // ... other cases
-//            default:
-//                return null;
-//        }
-//    }
 
     private void insertAnimalIntoDB(Animals animal) {
         String sql = "INSERT INTO nursery (name, dateBirth, skills) VALUES (?, ?, ?)";
-        try (Connection conn = this.connect();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, animal.getName());
-            pstmt.setDate(2, java.sql.Date.valueOf(animal.getDateBirth()));
-            pstmt.setString(3, animal.getSkills());
-            pstmt.executeUpdate();
+        try (Connection connection = this.connect();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, animal.getName());
+            preparedStatement.setDate(2, java.sql.Date.valueOf(animal.getDateBirth()));
+            preparedStatement.setString(3, animal.getSkills());
+            preparedStatement.executeUpdate();
             System.out.println("Animal added to database successfully.");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
@@ -129,79 +106,4 @@ public class DataBase {
         }
         return dateBirth;
     }
-//    Service service = new Service();
-//
-//
-//    static {
-//        try {
-//            Class.forName("org.mysql.Driver");
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//
-//        try {
-//            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    public List<Animals> index() {
-//        List<Animals> animals = new ArrayList<>();
-//
-//        try {
-//            Statement statement = connection.createStatement();
-//            String SQL = "SELECT * FROM nursery";
-//            ResultSet resultSet = statement.executeQuery(SQL);
-//
-//            while (resultSet.next()) {
-//                Animals animal = service.addNewAnimal();
-//                animal.setId(resultSet.getLong("id"));
-//                animal.setName(resultSet.getString("name"));
-//                animal.setDateBirth(resultSet.getDate("age").toLocalDate());
-//                animal.setSkills(resultSet.getString("skills"));
-//
-//                animals.add(animal);
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//        return animals;
-//    }
-//
-//    public Animals show(int id) {
-//        Animals animals = null;
-//        try {
-//            PreparedStatement preparedStatement =
-//                    connection.prepareStatement("SELECT * FROM nursery WHERE id=?");
-//            preparedStatement.setInt(1, id);
-//            ResultSet resultSet = preparedStatement.executeQuery();
-//
-//            resultSet.next();
-//            animals = service.addNewAnimal();
-//
-//            animals.setId(resultSet.getInt("id"));
-//            animals.setName(resultSet.getString("name"));
-//            animals.setDateBirth(resultSet.getDate("age").toLocalDate());
-//            animals.setSkills(resultSet.getString("skills"));
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//        return animals;
-//    }
-//
-//    public void save(Animals animals) {
-//        try {
-//            PreparedStatement preparedStatement =
-//                    connection.prepareStatement("INSERT INTO nursery VALUES(?,?,?)");
-//            preparedStatement.setString(1, animals.getName());
-//            preparedStatement.setDate(2, Date.valueOf(animals.getDateBirth()));
-//            preparedStatement.setString(3, animals.getSkills());
-//
-//            preparedStatement.executeUpdate();
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
--- src/main/java/com/sylman/nursery/human_friends/model/Service.java
+++ src/main/java/com/sylman/nursery/human_friends/model/Service.java
@@ -2,8 +2,6 @@ package com.sylman.nursery.human_friends.model;
 
 import com.sylman.nursery.human_friends.model.animals.*;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Service {
@@ -18,39 +16,6 @@ public class Service {
     public Animals addNewAnimal() {
         dataBase = new DataBase();
         dataBase.addNewAnimal();
-//        System.out.println("What name is gonna be");
-//        String name = scanner.nextLine();
-//
-//        LocalDate dateBirth = checkDate();
-//
-//        System.out.println("What skills does he/she has");
-//        String skills = scanner.nextLine();
-//
-//        System.out.println("Select the class: ");
-//        System.out.println("1. Cat");
-//        System.out.println("2. Dog");
-//        System.out.println("3. Hamster");
-//        System.out.println("4. Camel");
-//        System.out.println("5. Donkey");
-//        System.out.println("6. Horse");
-//        int user = scanner.nextInt();
-//        scanner.nextLine();
-//
-//        switch (user) {
-//            case 1 -> animals = new Cat(name, dateBirth, skills);
-//            case 2 -> animals = new Dog(name, dateBirth, skills);
-//            case 3 -> animals = new Hamster(name, dateBirth, skills);
-//            case 4 -> animals = new Camel(name, dateBirth, skills);
-//            case 5 -> animals = new Donkey(name, dateBirth, skills);
-//            case 6 -> animals = new Horse(name, dateBirth, skills);
-//            default -> {
-//                System.out.println("Wrong input");
-//            }
-//        }
-//
-//        animals.setName(name);
-//        animals.setDateBirth(dateBirth);
-//        animals.setSkills(skills);
 
         return dataBase.addNewAnimal();
     }
@@ -70,30 +35,5 @@ public class Service {
         Scanner scanner = new Scanner(System.in);
         String newSkill = scanner.nextLine();
         animals.setSkills(newSkill);
-    }
-
-    public LocalDate checkDate() {
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-//        try {
-//            return LocalDate.parse(dateOfBirth, formatter);
-//        } catch (DateTimeParseException e) {
-//            throw new DateException();
-//        }
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        boolean validInput = false;
-        LocalDate dateBirth = null;
-
-        while (!validInput) {
-            try {
-                System.out.print("Enter date of birth IN FORMAT: YYYY-MM-DD:\n");
-                String input = scanner.nextLine();
-                dateBirth = LocalDate.parse(input, formatter);
-                validInput = true;
-            } catch (Exception e) {
-                System.err.println("Incorrect format of date. Try again.");
-                scanner.nextLine();
-            }
-        }
-        return dateBirth;
     }
 }
